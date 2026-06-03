dbf4391e75d362aaebd4ca2c045f1803bd6bbde1
ericawong818-sketch <ericawong818@gmail.com>
2026-06-03 22:41:20 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// 這個檔案專門用來存放你的雲端設定
+const firebaseConfig = {
+    apiKey: "AIzaSyDHQLajKkJnQk9vyxbNjDWSkyp_TbDZ6M4",
+    authDomain: "storyrunner-86e7c.firebaseapp.com",
+    projectId: "storyrunner-86e7c",
+    storageBucket: "storyrunner-86e7c.firebasestorage.app",
+    messagingSenderId: "470467750762",
+    appId: "1:470467750762:web:756fc695f91e77fac21313"
+};
+
+// 讓主程式可以讀取到它
+export default firebaseConfig;
